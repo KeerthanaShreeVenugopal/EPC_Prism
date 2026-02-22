24e9f3b681f4f2d91c9fb768218594054587b035
Pallavi-N10 <pallavi.nandakishore10@gmail.com>
2026-02-22 21:03:42 +0530

    Initial figma

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import QueryEnginePage from './pages/QueryEnginePage';
+import DocumentAnalysisPage from './pages/DocumentAnalysisPage';
+import RiskAssessmentPage from './pages/RiskAssessmentPage';
+import AnalyticsDashboardPage from './pages/AnalyticsDashboardPage';
+import SupplyChainPage from './pages/SupplyChainPage';
+import KnowledgeBasePage from './pages/KnowledgeBasePage';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/query-engine" element={<QueryEnginePage />} />
+        <Route path="/document-analysis" element={<DocumentAnalysisPage />} />
+        <Route path="/risk-assessment" element={<RiskAssessmentPage />} />
+        <Route path="/analytics-dashboard" element={<AnalyticsDashboardPage />} />
+        <Route path="/supply-chain" element={<SupplyChainPage />} />
+        <Route path="/knowledge-base" element={<KnowledgeBasePage />} />
+      </Routes>
+    </Router>
+  );
+}
